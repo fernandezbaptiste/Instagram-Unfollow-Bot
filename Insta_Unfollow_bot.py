59c1e50a7678ae3551874a99cd88846a3496768b
Baptiste-Fernandez <fernandez.baptiste1@gmail.com>
2021-12-28 01:19:04 +0100

    current status

--- Insta_Unfollow_bot.py
+++ Insta_Unfollow_bot.py
@@ -1,4 +1,11 @@
- # Importing Modules
+#### PLAN ####
+# 1. Connect on Website
+# 2. Login with your username + Password
+# 3. Get to your profile and click on followers
+# 4. Scroll all the way down to your first followers (they are the most irrelevant ones)
+# 5. Start unfollowing one by one
+
+# Importing Modules
 try:
     from time import sleep
     import requests
@@ -7,6 +14,7 @@ try:
     from webdriver_manager.chrome import ChromeDriverManager
     from selenium.webdriver.common.keys import Keys
     import json
+    import re
 except Exception as e:
     print('Error importing: ', e,'\n')
 
@@ -15,7 +23,6 @@ with open('creds.json') as json_file:
     data = json.load(json_file)
 username = data['username']
 password = data['pass']
-
 
 #Opening Chrome Window
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -49,10 +56,10 @@ driver.find_element_by_xpath('/html/body/div[6]/div/div/div[3]/ul/div/li[10]').c
 sleep(5)
 
 
-
-# To go down till the end
+# To scroll down to the last user I need to click on a profile so it loads the other users
 # for i in range(96):
-for i in range(1,30):
+# for i in range(1,30):
+for i in range(5):
     # i = 1 -> li 12
     # i = 2 -> li 24
     # i = 3 -> li 36
@@ -61,32 +68,40 @@ for i in range(1,30):
         driver.find_element_by_xpath(html_string).click()
         print('Current link is:', html_string)
         sleep(5)
+
+    # Allow for 2 exceptions as sometimes there are bugs
     except Exception as e:
         try:
             sleep(5)
             html_string = "/html/body/div[6]/div/div/div[3]/ul/div/li[" + str(i*12) +"]"
             print('Retrying for link: ', html_string)
             driver.find_element_by_xpath(html_string).click()
+
         except Exception as e:
-            sleep(5)
-            driver.find_element_by_xpath("/html/body/div[6]/div/div/div[2]/a[2]").click()
-            sleep(5)
-            driver.find_element_by_xpath("/html/body/div[6]/div/div/div[2]/a[1]").click()
+            print('Error happened a thrid time!')
             sleep(5)
             html_string = "/html/body/div[6]/div/div/div[3]/ul/div/li[" + str(i*12) +"]"
             print('Retrying for link: ', html_string)
             driver.find_element_by_xpath(html_string).click()
+            # sleep(5)
+            # driver.find_element_by_xpath("/html/body/div[6]/div/div/div[2]/a[2]").click()
+            # sleep(5)
+            # driver.find_element_by_xpath("/html/body/div[6]/div/div/div[2]/a[1]").click()
+            # sleep(5)
+            # html_string = "/html/body/div[6]/div/div/div[3]/ul/div/li[" + str(i*12) +"]"
+            # print('Retrying for link: ', html_string)
+            # driver.find_element_by_xpath(html_string).click()
 
 
+# html_string = "/html/body/div[6]/div/div/div[3]/ul/div/li[12]"
 
-html_string = "/html/body/div[6]/div/div/div[3]/ul/div/li[12]"
 print('This is the last html from bottom of list: ', html_string)
 numbers_inside_html_link = (re.findall('\d+', html_string ))
+
 follower_rank = int(numbers_inside_html_link[2])
-
 while follower_rank < 340:
-
-    follower_html_to_unfollow =  "/html/body/div[6]/div/div/div[3]/ul/div/li[" + str(html_string[3]) + "]/div/div[2]/button"
+    print('I AM AT THIS POINT NOW')
+    follower_html_to_unfollow =  "/html/body/div[6]/div/div/div[3]/ul/div/li[" + str(follower_rank) + "]/div/div[2]/button"
 
     # unfollowing button click
     driver.find_element_by_xpath(follower_html_to_unfollow).click()
@@ -95,3 +110,10 @@ while follower_rank < 340:
     driver.find_element_by_xpath("/html/body/div[7]/div/div/div/div[3]/button[1]").click()
     sleep(5)
     follower_rank -= 1
+
+
+# Milena = /html/body/div[6]/div/div/div[3]/ul/div/li[39]/div/div[2]/button
+# Milena_Unfollow = /html/body/div[7]/div/div/div/div[3]/button[1]
+#
+# Sven = /html/body/div[6]/div/div/div[3]/ul/div/li[40]/div/div[2]/button
+#         /html/body/div[6]/div/div/div[3]/ul/div/li[40]/div/div[1]
